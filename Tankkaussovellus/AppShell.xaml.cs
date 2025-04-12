7d9040e87b1b826beadfea2502ce40a205088f37
Nhu Ha <hamynhu161@gmail.com>
2025-04-12 21:20:20 +0300

    Add .gitignore to exclude unnecessary files

---
+++ Tankkaussovellus/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Tankkaussovellus
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tankkaussovellus/Tankkaus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tankkaussovellus
+{
+    public class Tankkaus
+    {
+        public string Pvm {  get; set; }
+        public int Kilometrit { get; set; }
+        public int Litraa { get; set; }
+        public double Summa { get; set; }
+
+        public static double LaskeKeskikulutus (Tankkaus edellinen, Tankkaus nykyinen)
+        {
+            int kilemetritEro = nykyinen.Kilometrit - edellinen.Kilometrit;
+            int litratEro = nykyinen.Litraa;
+            double keskiKulutus = (double)litratEro / kilemetritEro;
+
+            if(kilemetritEro > 0)
+            {
+                return keskiKulutus;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
